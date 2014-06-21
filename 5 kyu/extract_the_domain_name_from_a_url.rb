7b450efef83f086782da9901e88dfe56da5ba62b
khchehab <khchehab@gmail.com>
2014-06-22 02:43:35 +0300

    Removed test code

--- 5 kyu/extract_the_domain_name_from_a_url.rb
+++ 5 kyu/extract_the_domain_name_from_a_url.rb
@@ -1,7 +1,3 @@
 def domain_name(url)
 	url.match(/(.*\.|.*\/\/)(.*)\..*/)[2]
 end
-
-puts domain_name("http://github.com/carbonfive/raygun")
-puts domain_name("http://www.zombie-bites.com")
-puts domain_name("https://www.cnet.com")
