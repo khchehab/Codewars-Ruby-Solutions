a663e628dcd811d409c243bb68bdf89de004d203
khchehab <aizen-qa@Khaleds-MacBook-Air.local>
2014-06-22 02:46:43 +0300

    Removed test codes

--- 5 kyu/extract_the_domain_name_from_a_url.rb
+++ 5 kyu/extract_the_domain_name_from_a_url.rb
@@ -1,7 +1,7 @@
 def domain_name(url)
 	url.match(/(.*\.|.*\/\/)(.*)\..*/)[2]
+<<<<<<< HEAD
 end
-
-puts domain_name("http://github.com/carbonfive/raygun")
-puts domain_name("http://www.zombie-bites.com")
-puts domain_name("https://www.cnet.com")
+=======
+end
+>>>>>>> FETCH_HEAD
